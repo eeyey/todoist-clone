1a3c1d34996fd551387be107974e6b049bcc5bd1
Boris Egorov <noname09011@gmail.com>
2023-01-20 12:43:38 +0500

    Create app loading state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
 
-import { AppRouter } from './components/common';
+import { AppRouter, PageLoader } from './components/common';
+import {
+  fetchProjects,
+  fetchTodos,
+  selectProjects,
+  selectTodos,
+  useAppDispatch,
+  useAppSelector,
+} from './core/store';
 
 function App() {
-  return <AppRouter />;
+  const dispatch = useAppDispatch();
+
+  const { status: projectsStatus } = useAppSelector(selectProjects);
+  const { status: todosStatus } = useAppSelector(selectTodos);
+
+  const isLoading = todosStatus === 'loading' || projectsStatus === 'loading';
+
+  React.useEffect(() => {
+    dispatch(fetchProjects());
+    dispatch(fetchTodos());
+  }, []);
+
+  return (
+    <>
+      {isLoading && <PageLoader />}
+      <AppRouter />
+    </>
+  );
 }
 
 export default App;
