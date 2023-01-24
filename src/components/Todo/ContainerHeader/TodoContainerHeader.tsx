df3617805e83a579a57f4bfb1c129a31c980ee73
Boris Egorov <noname09011@gmail.com>
2023-01-24 10:43:22 +0500

    TodoContainerHeader: Remove sticky prop and change setting sticky top

--- src/components/Todo/ContainerHeader/TodoContainerHeader.tsx
+++ src/components/Todo/ContainerHeader/TodoContainerHeader.tsx
@@ -8,16 +8,36 @@ import './TodoContainerHeader.css';
 
 interface TodoContainerHeaderProps {
   date: Date;
-  sticky?: number;
 }
 
 export const TodoContainerHeader: React.FC<TodoContainerHeaderProps> = (
   props,
 ) => {
-  const { date, sticky = 43 } = props;
+  const { date } = props;
+
+  const [sticky, setSticky] = React.useState(0);
+
+  const header = React.useRef<HTMLDivElement | null>(null);
+
+  React.useEffect(() => {
+    if (!header.current) return;
+
+    const content = header.current.closest('.container');
+    const stickyElement = content?.firstElementChild;
+
+    if (!stickyElement) return;
+
+    const { bottom: sticky } = stickyElement.getBoundingClientRect();
+
+    setSticky(sticky);
+  }, []);
 
   return (
-    <div style={{ top: `${sticky}px` }} className="todo-container-header">
+    <div
+      ref={header}
+      style={{ top: `${sticky}px` }}
+      className="todo-container-header"
+    >
       <h4 className="todo-container-name">
         {+date < +TODAY ? 'Просрочено' : getHeaderName(date)}
       </h4>
